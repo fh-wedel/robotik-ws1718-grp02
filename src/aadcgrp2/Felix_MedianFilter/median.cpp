c04654f026c785d20f8930168988fe99fe6c4a7e
Felix Maaß <felix@maass24.de>
2017-12-12 14:50:56 +0100

    Outsource the core C++ median filter.

---
+++ src/aadcgrp2/Felix_MedianFilter/median.cpp
@@ -0,0 +1,36 @@
+#include "median.h"
+
+cMedian::cMedian(int windowSize) {
+    this->windowSize = windowSize;
+}
+
+void cMedian::addValue(float newValue) {
+    // erase oldest values if window size has been reached
+    int amountOfOverflownElements = storedValues.size() - windowSize;
+    if (amountOfOverflownElements > 0) {
+        storedValues.erase(
+            storedValues.begin(),
+            storedValues.begin() + amountOfOverflownElements
+        );
+    }
+
+    // append new value
+    storedValues.push_back(newValue);
+}
+
+float cMedian::calculateMedian() {
+    std::vector<float> values = storedValues;
+
+    // sort values ascending
+    std::sort(values.begin(), values.end());
+
+    // 0 / 2 = 0    -> 0
+    // 1 / 2 = 0.5  -> 0
+    // 2 / 2 = 1    -> 1
+    // 3 / 2 = 1.5  -> 1
+    // 4 / 2 = 2    -> 2
+    // 5 / 2 = 2.5  -> 2
+    int medianIndex = values.size() / 2;
+
+    return values[medianIndex];
+}
---
+++ src/aadcgrp2/Felix_MedianFilter/median.h
@@ -0,0 +1,25 @@
+#ifndef _Median_H_
+#define _Median_H_
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <vector>
+#include <algorithm>
+
+class cMedian {
+
+public:
+    cMedian(int windowSize);
+
+    /*! the number of values to consider */
+    int windowSize;
+
+    /*! the most up to date values */
+    std::vector<float> storedValues;
+
+    void addValue(float);
+
+    float calculateMedian();
+};
+
+#endif // _Median_H_
