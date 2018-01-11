5f839ed36fddb12183597dad6e2b774e12e49b13
FHW-AADC-Grp2 <ite101754@stud.fh-wedel.de>
2018-01-11 18:16:31 +0100

    Remove intitial dummy project c files.

--- src/aadcgrp2/AADC_grp2/opencv.cpp
+++
@@ -1,5 +0,0 @@
-cv::Mat hoe_OpenCV(cv::Mat inputImage, float intensity) {
-    cv::Mat outputImage = inputImage.clone();
-    cv::circle(outputImage, cv::Point(200,200), 200, cv::Scalar(0,cv::saturate_cast<uchar>(intensity*4),0),-1);
-    return outputImage;
-}
